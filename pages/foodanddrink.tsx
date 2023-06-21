850a9db27e0370b5399cc78cba947cfc7df34156
Jonathan Routley <jonathan.wei.liang@gmail.com>
2023-06-21 12:56:30 +0100

    added some resizing of tables for mobile

--- pages/foodanddrink.tsx
+++ pages/foodanddrink.tsx
@@ -2,9 +2,12 @@ import Header from './components/header'
 import Socials from './components/socials'
 import "@fontsource/arvo"; // Defaults to weight 400.
 
+import Link from 'next/link';
+
 import { getDataFromSheets } from './api/sheets';
 import type { SheetItem } from './api/sheets';
 import type { GetStaticProps } from 'next';
+import { useWindowSize } from "@uidotdev/usehooks";
 
 function selectColumns(rows: any[][], nameCol: number, priceCol: number) {
   if (rows && rows.length > 0) {
@@ -50,28 +53,38 @@ function makeTable(data: SheetItem[], numCols: number) {
 
 
 export default function FoodAndDrink({ sheet }: { sheet: any[][]} ) {
+  const size = useWindowSize();
+  let numCols = 1
+  if (size.width > 640) {
+    numCols = 2
+  }  
   // const data = selectColumns(0, 1)
   const drinkData = selectColumns(sheet, 0, 1);
-  const drinkTableRows = makeTable(drinkData, 2);
+  const drinkTableRows = makeTable(drinkData, numCols);
   // console.log(drinkTableRows)
 
   const foodData = selectColumns(sheet, 3, 4);
-  const foodTableRows = makeTable(foodData, 2);
+  const foodTableRows = makeTable(foodData, numCols);
 
   const cakeData = selectColumns(sheet, 6, 7);
   const cakeTableRows = makeTable(cakeData, 1);
 
   const donutData = selectColumns(sheet, 9, 10);
-  const donutTableRows = makeTable(donutData, 2);
+  const donutTableRows = makeTable(donutData, numCols);
 
   return (
     <>
     <Header />
-    <main className="">
-      <div className="flex flex-col mx-auto bg-zinc-50 text-zinc-800 p-2 md:p-4 justify-start max-w-2xl w-full gap-4 md:gap-6">
+    <main>
+      <div className="flex flex-col mx-auto bg-zinc-50 text-zinc-800 p-2 md:p-4 justify-start max-w-2xl w-full gap-8 md:gap-6">
+        <div>
+          <Link href="/" className="text-slate-700 hover:text-slate-500">
+            ← Back to Home
+          </Link>
+        </div>
         <div className="flex flex-col gap-2">
           <h2 className="text-center">Drinks ☕</h2>
-            <table className="table-auto w-full border-separate border-spacing-y-2 border-spacing-x-8 bg-white shadow-md rounded-md">
+            <table className="table-auto w-3/4 mx-auto md:w-full border-separate border-spacing-y-2 border-spacing-x-8 bg-white shadow-md rounded-md">
               <tbody>
                 {drinkTableRows}
               </tbody>
@@ -79,7 +92,7 @@ export default function FoodAndDrink({ sheet }: { sheet: any[][]} ) {
         </div>
         <div className="flex flex-col gap-2">
           <h2 className="text-center">Food 🍴</h2>
-          <table className="table-auto w-full border-separate border-spacing-y-2 border-spacing-x-8 bg-white shadow-md rounded-md">
+          <table className="table-auto w-3/4 mx-auto md:w-full border-separate border-spacing-y-2 border-spacing-x-8 bg-white shadow-md rounded-md">
               <tbody>
                 {foodTableRows}
               </tbody>
@@ -87,7 +100,7 @@ export default function FoodAndDrink({ sheet }: { sheet: any[][]} ) {
         </div>
         <div className="flex flex-col gap-2">
           <h2 className="text-center">Cakes to Order 🍰</h2>
-          <table className="table-fixed text-center m-auto w-2/3 border-separate bg-white shadow-md rounded-md">
+          <table className="table-fixed text-center m-auto w-3/4 border-separate bg-white shadow-md rounded-md">
               <tbody>
                 {cakeTableRows}
               </tbody>
@@ -95,7 +108,7 @@ export default function FoodAndDrink({ sheet }: { sheet: any[][]} ) {
         </div>
         <div className="flex flex-col gap-2">
           <h2 className="text-center">Donuts 🍩</h2>
-          <table className="table-auto w-full border-separate border-spacing-y-2 border-spacing-x-8 bg-white shadow-md rounded-md">
+          <table className="table-auto w-3/4 mx-auto md:w-full border-separate border-spacing-y-2 border-spacing-x-8 bg-white shadow-md rounded-md">
               <tbody>
                 {donutTableRows}
               </tbody>
